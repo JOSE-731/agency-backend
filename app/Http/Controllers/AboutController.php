df725a07fd06de22a8d3a9035fc36266e6c197b9
jose diaz <jose7312000@gmail.com>
2022-02-21 21:07:33 -0500

    Agregando rutas y modificando el controlador de About

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\About;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    //
+    public function index()
+    {
+        //Mostramos todos los datos
+        $about = About::all();
+
+        return $about;
+    }
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request){
+
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request){
+
+        $about = About::findOrFail($request->id);
+
+    }
+
+    public function destroy(Request $request){
+
+        $about = About::deleted($request->id);
+    
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Rutas de about
+Route::get('/about', [AboutController::class,'index']);
+Route::post('/about', [AboutController::class,'store']);
+Route::put('/about/{id}', [AboutController::class,'update']);
+Route::delete('/about/{id}', [AboutController::class,'destroy']);
